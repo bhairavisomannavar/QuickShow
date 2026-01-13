31dc11f7a83fdd0ccd5d3dd78ab3cb300afd513b
Bhairavi <bhairavisomannavar@gmail>
2026-01-13 16:57:01 +0530

    first commit

---
+++ client/src/pages/home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import HeroSection from '../components/HeroSection'
+import FeaturedSection from '../components/FeaturedSection'
+import TrailerSection from '../components/TrailerSection'
+
+function home() {
+  return (
+    <>
+    <HeroSection/>
+    <FeaturedSection/>
+    <TrailerSection />
+
+    </>
+  )
+}
+
+export default home
